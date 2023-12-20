a9f25ca7d2b817a6770c377625f606adc0df53cd
Haidar Obeid <haidar.obeid@universalfieldrobots.com.au>
2023-12-20 14:48:47 +1000

    clean

--- test.py
+++ test.py
@@ -2,13 +2,19 @@ import os
 import time
 from datetime import datetime
 
+# Specify the folder
+folder = "files"
+
+# Create the folder if it doesn't exist
+os.makedirs(folder, exist_ok=True)
+
 for i in range(5000):
     # Get the current date and time
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d%H%M%S")
 
     # Create a new file with the current timestamp
-    with open(f"{timestamp}.txt", "w") as file:
+    with open(f"{folder}/{timestamp}.txt", "w") as file:
         file.write(f"File created at {now}")
 
     # Add the new file to the repository
