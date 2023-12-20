009f26193f5cf2c6e6d514f721ef3533c1235c13
Haidar Obeid <haidar.obeid@universalfieldrobots.com.au>
2023-12-20 14:42:35 +1000

    clean

--- test.py
+++ test.py
@@ -2,19 +2,22 @@ import os
 import time
 from datetime import datetime
 
-# Get the current date and time
-now = datetime.now()
-timestamp = now.strftime("%Y%m%d%H%M%S")
+for i in range(5000):
+    # Get the current date and time
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
 
-# Create a new file with the current timestamp
-with open(f"{timestamp}.txt", "w") as file:
-    file.write(f"File created at {now}")
+    # Create a new file with the current timestamp
+    with open(f"{timestamp}.txt", "w") as file:
+        file.write(f"File created at {now}")
 
-# Add the new file to the repository
-os.system(f"git add {timestamp}.txt")
+    # Add the new file to the repository
+    os.system(f"git add {timestamp}.txt")
 
-# Commit the changes
-os.system(f'git commit -m "Added file at {now}"')
+    # Commit the changes
+    os.system(f'git commit -m "Added file at {now}"')
 
-# Push the changes to the repository
-os.system("git push origin main")
+    # # Push the changes to the repository
+    # os.system("git push origin main")
+
+    print(f"Added file at {now}")
